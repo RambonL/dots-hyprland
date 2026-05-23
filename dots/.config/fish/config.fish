abd59aef455a2ee53d1d391be75ce23ee63c03ce
RambonL <renderman99@hotmail.de>
2026-05-23 22:59:43 +0200

    fix: add ~/.npm-global/bin and ~/.local/bin to PATH in config.fish

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -35,5 +35,4 @@ if status is-interactive
         alias ssh 'kitten ssh'
     end
 end
-set -x PATH ~/.npm-global/bin $PATH
-export PATH="$HOME/.local/bin:$PATH"
+set -x PATH ~/.npm-global/bin ~/.local/bin $PATH
